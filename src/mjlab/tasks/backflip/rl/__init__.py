53a8e1ed9e30a3149151e08f79af32a62719302e
TH5478 <114805734+TH5478@users.noreply.github.com>
2025-12-19 22:08:05 -0800

    Add files via upload

---
+++ src/mjlab/tasks/backflip/rl/__init__.py
@@ -0,0 +1,10 @@
+from mjlab.tasks.backflip.rl.exporter import (
+  attach_onnx_metadata as attach_onnx_metadata,
+)
+from mjlab.tasks.backflip.rl.exporter import (
+  export_backflip_policy_as_onnx as export_backflip_policy_as_onnx,
+)
+from mjlab.tasks.backflip.rl.runner import (
+  BackflipOnPolicyRunner as BackflipOnPolicyRunner,
+)
+
